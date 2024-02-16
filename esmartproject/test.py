c6190953c5280b6438935c6cf8d207f3a22d21c5
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2024-02-16 09:25:02 +0530

    v1

---
+++ esmartproject/test.py
@@ -0,0 +1,80 @@
+'''
+### Resposeive
+laptop screen + mobile screen
+
+### Model setup
+
+
+atharva:123
+- studentmodel
+id
+name
+roll no
+division
+engyear
+branch
+cgpa
+sgpa
+year
+
+
+- mksstudentmodel
+id
+roll no : 6753
+exam    : ut / sem
+sub     : ads / sma
+mrk     : 16  / 72
+outof   : 20 / 80
+
+
+- attendencemodel
+id
+roll no
+
+# division
+# engyear
+# branch
+# year
+
+subject
+starttime
+endtime
+status
+
+
+teachads:123
+- teachermodel
+id
+name
+
+branch
+division
+subject
+engyear
+year
+
+
+hodcomps:123
+- hodmodel (hod register the teacher and assign role)
+id
+name
+branch
+year
+
+
+
+### Dashboard: based on current year
+- total classes  register list
+- total students register list
+- classes cards
+    - branch
+    - engyear
+    - student count
+    - subject
+    - total attendence rate : %
+    - 
+
+- per class attendence percentage count
+- 
+
+'''
